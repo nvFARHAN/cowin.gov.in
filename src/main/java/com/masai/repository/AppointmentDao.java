5419c27997645ec9557b4244d33917b36e6795a6
Shivam Maheshwari <shivam.maheshwari796@gmail.com>
2022-08-11 16:50:03 +0530

    Add dao interfaces connected with member class

---
+++ src/main/java/com/masai/repository/AppointmentDao.java
@@ -0,0 +1,9 @@
+package com.masai.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.masai.model.Appointment;
+
+public interface AppointmentDao extends JpaRepository<Appointment, Integer> {
+
+}
