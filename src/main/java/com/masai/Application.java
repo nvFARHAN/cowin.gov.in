8364de5c623cbbf10d9741017be3ba1925411b2f
nvFARHAN <nawaz.farhan26@gmail.com>
2022-08-16 21:52:00 +0530

    Swagger

--- src/main/java/com/masai/Application.java
+++ src/main/java/com/masai/Application.java
@@ -3,7 +3,11 @@ package com.masai;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @SpringBootApplication
+@EnableSwagger2
+//@Configuration
 public class Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/masai/controller/LoginController.java
+++ src/main/java/com/masai/controller/LoginController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,36 +14,36 @@ import com.masai.service.AdminLogInServiceImpl;
 import com.masai.service.UserLogInServiceImpl;
 
 @RestController
+@RequestMapping("/login")
 public class LoginController {
 
 	@Autowired
 	private UserLogInServiceImpl userLogInServiceImpl;
-	
+
 	@Autowired
 	private AdminLogInServiceImpl adminLogInServiceImpl;
-	
-	//for user login
+
+	// for user login
 	@PostMapping("/userlogin")
 	public String logInUser(@RequestBody UserDTO userDTO) throws Exception {
 		return userLogInServiceImpl.logIntoAccount(userDTO);
 	}
-	
-	//for user logout
+
+	// for user logout
 	@PatchMapping("/userlogout")
-	public String logOutUser(@RequestParam(required=false) String key) {
+	public String logOutUser(@RequestParam(required = false) String key) {
 		return userLogInServiceImpl.logOutAccount(key);
 	}
-	
-	
-		//for admin login
-		@PostMapping("/adminlogin")
-		public String logInAdmin(@RequestBody AdminDTO adminDTO) {
-			return adminLogInServiceImpl.logIntoAccount(adminDTO);
-		}
-		
-		//for admin logout
-		@PatchMapping("/adminlogout")
-		public String logOutAdmin(@RequestParam(required=false) String key) {
-			return adminLogInServiceImpl.logOutAccount(key);
-		}
+
+	// for admin login
+	@PostMapping("/adminlogin")
+	public String logInAdmin(@RequestBody AdminDTO adminDTO) {
+		return adminLogInServiceImpl.logIntoAccount(adminDTO);
+	}
+
+	// for admin logout
+	@PatchMapping("/adminlogout")
+	public String logOutAdmin(@RequestParam(required = false) String key) {
+		return adminLogInServiceImpl.logOutAccount(key);
+	}
 }
