507a687afe8ed3053578e214495d02c4e61995b6
moh shahrukh khan <shahrukhturk9@gmail.com>
2022-08-16 20:51:31 +0530

    added Swagger

--- src/main/java/com/masai/controller/LoginController.java
+++ src/main/java/com/masai/controller/LoginController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,7 @@ import com.masai.service.login.AdminLogInServiceImpl;
 import com.masai.service.login.UserLogInServiceImpl;
 
 @RestController
+@RequestMapping("/api")
 public class LoginController {
 
 	@Autowired
